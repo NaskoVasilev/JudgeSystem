23a5a6690c138967dc4e52e149b25f2e666b0fe8
NaskoVasilev <nasko_vasilev@abv.bg>
2019-06-24 10:13:23 +0300

    Create course tree component to list all courses with their subcategories

--- Web/JudgeSystem.Web/Components/CoursesTree.cs
+++ Web/JudgeSystem.Web/Components/CoursesTree.cs
@@ -1,8 +1,25 @@
 ﻿namespace JudgeSystem.Web.Components
 {
-	using Microsoft.AspNetCore.Mvc;
+    using JudgeSystem.Data.Models.Enums;
+    using JudgeSystem.Services.Data;
+    using JudgeSystem.Web.Infrastructure.Extensions;
+
+    using Microsoft.AspNetCore.Mvc;
 
 	public class CoursesTree : ViewComponent
 	{
+		private readonly ICourseService courseService;
+
+		public CoursesTree(ICourseService courseService)
+		{
+			this.courseService = courseService;
+		}
+
+		public IViewComponentResult Invoke()
+		{
+			var courses = courseService.All();
+			ViewData["lessonTypes"] = EnumExtensions.GetEnumValuesAsString<LessonType>();
+			return View(courses);
+		}
 	}
 }
