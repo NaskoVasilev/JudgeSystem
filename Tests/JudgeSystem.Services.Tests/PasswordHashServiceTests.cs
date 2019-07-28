59bd954e42c5c699c4c96ceb5ef2797d3997724a
NaskoVasilev <nasko_vasilev@abv.bg>
2019-07-28 21:59:04 +0300

    PasswordHashService Tests

---
+++ Tests/JudgeSystem.Services.Tests/PasswordHashServiceTests.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+
+namespace JudgeSystem.Services.Tests
+{
+    public class PasswordHashServiceTests
+    {
+        private readonly PasswordHashService passwordHashService;
+
+        public PasswordHashServiceTests()
+        {
+            this.passwordHashService = new PasswordHashService();
+        }
+
+        [Fact]
+        public void HashPassword_AfterHashing_OriginalValuesAndHashedValueMustBeDifferent()
+        {
+            string value = "password";
+
+            string hash = passwordHashService.HashPassword(value);
+
+            Assert.True(hash != value);
+        }
+
+        [Fact]
+        public void HashPassword_HashSameValueTwice_ShouldProduceEqualHashes()
+        {
+            string value = "password";
+
+            string firstHash = passwordHashService.HashPassword(value);
+            string secondHash = passwordHashService.HashPassword(value);
+
+            Assert.Equal(firstHash, secondHash);
+        }
+    }
+}
