1c0e9c629f9a275780991f6bb48a8edd18376ed5
NaskoVasilev <nasko_vasilev@abv.bg>
2019-08-04 14:22:36 +0300

    Remove Privacy action and delete Privacy.cshtml

--- Web/JudgeSystem.Web/Controllers/HomeController.cs
+++ Web/JudgeSystem.Web/Controllers/HomeController.cs
@@ -9,11 +9,6 @@
             return this.View();
         }
 
-        public IActionResult Privacy()
-        {
-            return this.View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() => this.View();
     }
