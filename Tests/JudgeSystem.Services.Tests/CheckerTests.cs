18b9a6d1d990a9b16e9db1a01f1f31d3e030aef0
Atanas Vasilev <nasko01vasilev@gmail.com>
2019-09-03 23:36:14 +0300

    Checker service unit tests

---
+++ Tests/JudgeSystem.Services.Tests/CheckerTests.cs
@@ -0,0 +1,35 @@
+﻿using JudgeSystem.Workers.Common;
+
+using Xunit;
+
+namespace JudgeSystem.Services.Tests
+{
+    public class CheckerTests
+    {
+        [Fact]
+        public void Check_WithUnsucessfullResult_ShouldSetIsCorrectToTrue()
+        {
+            var checker = new Checker();
+            var executionResult = new ExecutionResult() { Error = "error" };
+
+            CheckerResult checkerResult = checker.Check(executionResult, null);
+
+            Assert.False(checkerResult.IsCorrect);
+        }
+
+        [Theory]
+        [InlineData("test", "test", true)]
+        [InlineData("test ", "test", true)]
+        [InlineData("test", "test  ", true)]
+        [InlineData("test\r\n", "test  ", true)]
+        public void Check_WithDifferentArguments_ShouldreturnDifferentResults(string output, string expectedOutput, bool isCorrect)
+        {
+            var checker = new Checker();
+            var executionResult = new ExecutionResult() { Output = output };
+
+            CheckerResult checkerResult = checker.Check(executionResult, expectedOutput);
+
+            Assert.Equal(isCorrect, checkerResult.IsCorrect);
+        }
+    }
+}
